395d736014f3ff3cdeda18bdd73527e060e855c7
jingwook <qnfdkf153@gmail.com>
2022-07-19 09:58:02 +0900

    3일차 인출학습 완료
    근데 반복문을 탈출해서 메인루프를 다시 돌리고 싶은데 불리언을 이용해서 탈출하는게 좋은 구조인가?

--- week02/NumberBaseball/NumberBaseball3.java
+++ week02/NumberBaseball/NumberBaseball3.java
@@ -17,17 +17,34 @@ public class NumberBaseball3 {
         int [] answer = new int[]{shuffleNumber[0], shuffleNumber[1], shuffleNumber[2]};
         int [] guess = new int[3];
 
-        boolean isOverlapped = false;
+        int strike = 0;
+        int ball = 0;
 
-        while (true) {
-            int strike = 0;
-            int ball = 0;
+        while (strike <3) {
+            strike= 0;
+            ball = 0;
+
+            boolean isOverlapped = false;
+            boolean isOutOfRange = false;
 
             //입력
             System.out.print("Guess number: ");
 
             for (int i=0; i<guess.length; i+=1){
+                guess[i] = 0;
                 guess[i] = scanner.nextInt();
+                if (guess[i] < 0 || guess[i] > 9 ){
+                    isOutOfRange =true;
+
+                    if (i == guess.length-1){
+                        break;
+                    }
+                }
+            }
+
+            if (isOutOfRange == true){
+                System.out.println("0~9까지의 숫자를 입력해주세요!");
+                continue;
             }
 
             //처리
@@ -35,7 +52,6 @@ public class NumberBaseball3 {
                 for (int j = 0; j < guess.length; j += 1){
                     if (guess[i] == guess[j] && i != j){
                         isOverlapped = true;
-                        break;
                     }
 
                     if (guess[i] == answer[i]){
@@ -52,9 +68,15 @@ public class NumberBaseball3 {
             //출력
             if (isOverlapped == true){
                 System.out.println("각각 다른 숫자를 입력해주세요!");
+                continue;
             }
+
             System.out.println(strike + " Strike");
             System.out.println(ball + " Ball");
+
+            if (strike == 3){
+                System.out.println("You win!!");
+            }
         }
     }
 }
