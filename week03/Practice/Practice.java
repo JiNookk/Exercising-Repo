5bc8b2c6968bba498b8d57a93bcd5534d8577cd2
jingwook <qnfdkf153@gmail.com>
2022-07-29 10:51:52 +0900

    코딩 테스트

--- week03/Practice/Practice.java
+++ week03/Practice/Practice.java
@@ -1,22 +1,37 @@
-import javax.swing.*;
-import java.awt.*;
-
 public class Practice {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Practice application = new Practice();
 
-        application.run();
+        int[][] arr1 = new int[][]{
+                {1, 2}
+                , {2, 3}};
+
+        int[][] arr2 = new int[][]{
+                {3, 4}
+                , {5, 6}};
+
+        int[][] answer = application.solution(arr1, arr2);
+
+        for (int i = 0; i < answer.length; i +=1){
+            for(int element:answer[i]){
+                System.out.println(element);
+            }
+        }
     }
 
-    private void run() {
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(590, 599);
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = {};
 
-        Container contentPane = frame.getContentPane();
-        contentPane.add(frame);
-        contentPane.add(new JLabel("Hi"));
+        int[][] temp = new int[arr1.length][arr1[1].length];
 
-        frame.setVisible(true);
+        for (int i = 0; i < arr1.length; i += 1) { //arr배열의 n번째 배열요소
+            for (int j = 0; j < arr1[i].length; j += 1) {    //arr배열의 배열의 n번째 요소
+                temp[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+
+        answer = temp;
+
+        return answer;
     }
 }
